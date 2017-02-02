2d64182bd6d3f979e91d439695f21a895088e99a
BobBard <borys.bardysh@synergy-way.com>
2017-02-02 18:13:03 +0200

    Ticket #1 fixing issues

--- apps/hello/views.py
+++ apps/hello/views.py
@@ -7,22 +7,17 @@ from apps.hello.models import UserData
 
 
 class HomeView(View):
-
     def get_user_data(self):
-        user = User.objects.get(username='Borys')
-        if not user:
-            user = User.objects.create(email='borys.bardysh@synergy-way.com',
-                                       username='Borys',
-                                       password='11061990')[0]
-        userdata = UserData.objects.get(user=user)
-        if not userdata:
-            userdata = UserData.objects.create(user=user,
-                                               surname="Bardysh",
-                                               bio="Developer at synergy-way",
-                                               birth_date="1990-11-06",
-                                               jabber="bobbard@42cc.co",
-                                               skype="id",
-                                               other_contacts="")[0]
+        user = User.objects.get_or_create(email='borys.bardysh@synergy-way.com',
+                                          username='Borys',
+                                          password='11061990')[0]
+        userdata = UserData.objects.get_or_create(user=user,
+                                                  surname="Bardysh",
+                                                  bio="Developer at synergy-way",
+                                                  birth_date="1990-11-06",
+                                                  jabber="bobbard@42cc.co",
+                                                  skype="id",
+                                                  other_contacts="")[0]
         result = dict(model_to_dict(user), **model_to_dict(userdata))
         return result
 
